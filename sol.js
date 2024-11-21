abd652f32b21e2200508b3bc30210f422b4b713c
Piyush5521 <piyushagarwal1408@gmail.com>
2024-11-21 22:57:36 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -11,4 +11,24 @@ if (num === revnum) {
     console.log("Not Palindrome");
 }
 
-// 
+// find max and min number from array
+let arr = [3, 1, 5, 7, -2, 9];
+
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+
+let max= arr[0];
+for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+}
+
+let min = arr[0];
+for (let i = 0; i < arr.length; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+}
+console.log("Maximum Number: " + max);
+console.log("Minimum Number: " + min);
