ea2fd6406dd615ef59ea040f26a91b4b43fed211
Piyush5521 <piyushagarwal1408@gmail.com>
2024-11-25 16:00:02 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -14,3 +14,17 @@ function fiveCharactersOrFewerOnly(arr) {
 }
 // test
 console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]));
+
+
+// Q) Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+function peopleWhoBelongToTheIlluminati(arr){
+      return arr.filter((n) => n.member === true );
+}
+// test
+console.log(peopleWhoBelongToTheIlluminati([
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: true },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]));
