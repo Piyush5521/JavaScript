e542e1ea583f45053cb982167a49ac518faa4fc1
Piyush5521 <piyushagarwal1408@gmail.com>
2024-11-25 15:54:45 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -7,3 +7,10 @@ function evensOnly(arr) {
 }
 // test
 console.log(evensOnly([3, 6, 8, 2]));
+
+// Q) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length      
+function fiveCharactersOrFewerOnly(arr) {
+  return arr.filter((n) => n.length <= 5);
+}
+// test
+console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]));
