933d1e294ca2c96ce371eb52a3145f839db5943c
Piyush <piyushagarwal1408@gmail.com>
2024-12-12 23:22:03 +0530

    Update index.js

--- tasks/Task12.1/index.js
+++ tasks/Task12.1/index.js
@@ -1,58 +1,27 @@
-let tables = $("#display_data");
+const tables = $("#display_data");
 
 const fetchData = async () => {
   const response = await fetch("json.json");
   const data = await response.json();
 
-  var str = "";
+  let str = "";
 
   $.each(data, function (key, value) {
     str += `<tr><td class="fieldName"> ${value.Name}</td>
-            <td class="fieldExt">${value["Ext."]}</td>
-            <td class="fieldCity">${value.City}</td>
-            <td class="fieldStartDate"> ${value["Start Date"]} </td>
-            <td class="fieldCompletion">${value.Completion}</td>
+              <td class="fieldExt">${value["Ext."]}</td>
+              <td class="fieldCity">${value.City}</td>
+              <td class="fieldStartDate"> ${value["Start Date"]} </td>
+              <td class="fieldCompletion">${value.Completion}</td>
             </tr>`;
   });
 
+  // Append the rows to the table body
   tables.html(str);
 
-  let table = new DataTable('#myTable')
-//   $(document).ready(function() {
-//     $(table).dataTable( {
-//         "pagingType": "simple_incremental_bootstrap"
-//     });
-//   });
-
-    //   $(document).ready(function() {
-    //       $('#myTable').dataTable( {
-    //           "pagingType": "bootstrap_input"
-    //       } );
-    //   });
-
-    $(document).ready(function() {
-        $(table).dataTable( {
-            "sPaginationType": "listbox"
-        } );
-    } );
-
+  // Initialize DataTable (apply it to the table with ID myTable)
+  $('#myTable').DataTable({
+    "sPaginationType": "listbox" // Selectbox pagination type
+  });
 };
+
 fetchData();
-
-
-
-//   $(document).ready(function() {
-//       $('#myTable').dataTable( {
-//           "pagingType": "bootstrap_input"
-//       } );
-//   });
-
-
-//   $("#myTable").DataTable({
-//     columnDefs: [
-//       {
-//         targets: 3,
-//         render: DataTable.render.moment("YYYY/DD/MM", "Do MMM YYYY"),
-//       },
-//     ],
-//   });
