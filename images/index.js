408f2ab5c90222c365e0a3083efe965bac9453f9
Piyush <piyushagarwal1408@gmail.com>
2024-12-25 23:44:29 +0530

    Update index.js

--- images/index.js
+++ images/index.js
@@ -4,12 +4,26 @@ const fetchData = async () => {
     const response = await fetch("images.json");
     const data = await response.json();
 
-    let str = "";
-    data.map((item) => {
-        str += `<img src="${item.url}" alt="${item.category} class="img" style="margin-right:5px;">`;
-    });
+    const displayImages = (data) => {
+        let str = "";
+        data.map((item) => {
+            str += `<img src="${item.url}" alt="${item.category} class="img" style="margin-right:5px;">`;
+        });
 
-    CardImage.html(str);
+        CardImage.html(str);
+    }
+
+    displayImages(data);
+
+    $("#car_btn").on("click", function(){
+        const filteredData = data.filter((item) => item.category === "car");
+        displayImages(filteredData);
+    })
+
+    $("#animal_btn").on("click", function(){
+        const filteredData = data.filter((item) => item.category === "animal");
+        displayImages(filteredData);
+    })
 }
 
 fetchData();
