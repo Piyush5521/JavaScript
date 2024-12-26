37e06fb6b703f0d3117a855189b3a68a429fd5cf
Piyush <piyushagarwal1408@gmail.com>
2024-12-26 15:18:36 +0530

    Update index.js

--- images/index.js
+++ images/index.js
@@ -1,5 +1,11 @@
 let CardImage = $(".card");
 
+// Modal elements
+const modal = $("#imageModal");
+const modalImage = $("#modalImage");
+const downloadBtn = $("#downloadBtn");
+const closeModal = $("#closeModal");
+
 const fetchData = async () => {
     const response = await fetch("images.json");
     const data = await response.json();
@@ -7,7 +13,10 @@ const fetchData = async () => {
     const displayImages = (data) => {
         let str = "";
         data.map((item) => {
-            str += `<img src="${item.url}" alt="${item.category.join(', ')}" class="img" style="margin-right:5px;">`;
+            // Display category's first value on hover
+            str += `<div class="image-container">
+                        <img src="${item.url}" alt="${item.category.join(', ')}" class="img" style="margin-right:5px;" data-category="${item.category[0]}" data-url="${item.url}">
+                    </div>`;
         });
 
         CardImage.html(str);
@@ -15,6 +24,7 @@ const fetchData = async () => {
 
     displayImages(data);
 
+    // Handle category filter buttons
     $("#car_btn").on("click", function(){
         const filteredData = data.filter((item) => item.category.includes("car"));
         displayImages(filteredData);
@@ -35,6 +45,7 @@ const fetchData = async () => {
         displayImages(filteredData);
     });
 
+    // Debounced search function
     const debounce = (func, delay) => {
         let timeoutId;
         return function(...args) {
@@ -53,7 +64,33 @@ const fetchData = async () => {
     $("#searchfor").on("input", debounce(function() {
         const query = $(this).val();
         searchImages(query);
-    }, 1000)); 
+    }, 1000));
+
+    // Hover effect to display the first category
+    $(document).on("mouseenter", ".img", function() {
+        const category = $(this).data("category");
+        $(this).attr("title", category);  // Show category on hover
+    });
+
+    // Image click to open in modal
+    $(document).on("click", ".img", function() {
+        const imageUrl = $(this).data("url");
+        modalImage.attr("src", imageUrl);  // Set image in modal
+        downloadBtn.attr("href", imageUrl);  // Set download link
+        modal.show();  // Show modal
+    });
+
+    // Close modal
+    closeModal.on("click", function() {
+        modal.hide();  // Hide modal
+    });
+
+    // Close modal when clicking outside of the modal content
+    $(window).on("click", function(event) {
+        if (event.target === modal[0]) {
+            modal.hide();
+        }
+    });
 }
 
 fetchData();
